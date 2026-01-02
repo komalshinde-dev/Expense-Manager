562a0dee1210ff40a1708618cbeb27568dc787b5
Komal Shinde <214840814+komalshinde-dev@users.noreply.github.com>
2026-01-02 21:10:03 +0530

    Update sips.js

--- frontend/src/api/sips.js
+++ frontend/src/api/sips.js
@@ -1,25 +1,67 @@
-import API from './axios';
+import axios from 'axios';
 
-// Create SIP
-export const createSIP = async (data) => {
-  const res = await API.post('/sips', data);
-  return res.data;
+const API_URL = 'https://expense-manager-backend-31gm.onrender.com/api/sips';
+
+// Get auth token from localStorage
+const getAuthHeader = () => {
+  const token = localStorage.getItem('token');
+  return token ? { Authorization: `Bearer ${token}` } : {};
+};
+
+// Create new SIP
+export const createSIP = async (sipData) => {
+  const response = await axios.post(API_URL, sipData, {
+    headers: getAuthHeader()
+  });
+  return response.data;
 };
 
 // Get all SIPs
-export const getSIPs = async () => {
-  const res = await API.get('/sips');
-  return res.data;
+export const getSIPs = async (activeOnly = true) => {
+  const response = await axios.get(API_URL, {
+    params: { active: activeOnly },
+    headers: getAuthHeader()
+  });
+  return response.data;
+};
+
+// Get single SIP with detailed analysis
+export const getSIP = async (id) => {
+  const response = await axios.get(`${API_URL}/${id}`, {
+    headers: getAuthHeader()
+  });
+  return response.data;
 };
 
 // Update SIP
-export const updateSIP = async (id, data) => {
-  const res = await API.put(`/sips/${id}`, data);
-  return res.data;
+export const updateSIP = async (id, sipData) => {
+  const response = await axios.put(`${API_URL}/${id}`, sipData, {
+    headers: getAuthHeader()
+  });
+  return response.data;
 };
 
 // Delete SIP
 export const deleteSIP = async (id) => {
-  const res = await API.delete(`/sips/${id}`);
-  return res.data;
+  const response = await axios.delete(`${API_URL}/${id}`, {
+    headers: getAuthHeader()
+  });
+  return response.data;
+};
+
+// Search for fund symbols
+export const searchFunds = async (query) => {
+  const response = await axios.get(`${API_URL}/search-funds`, {
+    params: { query },
+    headers: getAuthHeader()
+  });
+  return response.data;
+};
+
+// Refresh SIP valuation
+export const refreshSIP = async (id) => {
+  const response = await axios.post(`${API_URL}/${id}/refresh`, {}, {
+    headers: getAuthHeader()
+  });
+  return response.data;
 };
